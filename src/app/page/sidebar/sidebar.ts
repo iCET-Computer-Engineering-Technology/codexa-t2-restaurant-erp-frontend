1bbf88f02676bbf6adcd72f6c1a09ec9d4a1ab69
AksaMJML <akaks0186@gmail.com>
2026-03-24 10:47:20 +0530

    refactor: remove unused RouterOutlet import from Sidebar component

--- src/app/page/sidebar/sidebar.ts
+++ src/app/page/sidebar/sidebar.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
