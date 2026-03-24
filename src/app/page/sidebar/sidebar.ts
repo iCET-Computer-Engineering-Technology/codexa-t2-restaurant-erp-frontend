c54b591cef6067d3f41ce1509968d54cba4f5099
Wasuda Sandaruvan <147605725+WasuVa@users.noreply.github.com>
2026-03-24 13:23:35 +0530

    feat: add RouterLink and RouterLinkActive imports to sidebar component

--- src/app/page/sidebar/sidebar.ts
+++ src/app/page/sidebar/sidebar.ts
@@ -5,7 +5,7 @@ import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
