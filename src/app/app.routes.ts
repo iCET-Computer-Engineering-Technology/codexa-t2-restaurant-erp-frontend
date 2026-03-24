1e176ff8e0c24b08180442753174077b6471027e
amilasuranjith-dev <amilasuranjith.dev@gmail.com>
2026-03-24 22:32:00 +0530

    feat: implement role-based authorization guard for route protection

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,8 @@ import { KitchenDashboard } from './page/kitchen/kitchen-dashboard/kitchen-dashb
 import { KitchenOrderTable } from './page/kitchen/kitchen-order-table/kitchen-order-table';
 import { OrderAssign } from './page/kitchen/order-assign/order-assign';
 import { OrderPlacementComponent } from './page/order-placement/order-placement.component';
+
+import { roleGuard } from './guards/role.guard';
 export const routes: Routes = [
   {
     path: '',
@@ -17,23 +19,31 @@ export const routes: Routes = [
   },
   {
     path: 'dashboard',
-
+    canActivate: [roleGuard],
     component: Dashboard,
   },
   {
     path: 'cashier',
+    canActivate: [roleGuard],
+    data: { roles: ['ROLE_ADMIN', 'ROLE_CASHIER'] },
     loadComponent: () => import('./page/cashier/cashier').then((m) => m.Cashier),
   },
   {
     path: 'waiter',
+    canActivate: [roleGuard],
+    data: { roles: ['ROLE_ADMIN', 'ROLE_WAITER'] },
     loadComponent: () => import('./page/waiter/waiter').then((m) => m.Waiter),
   },
   {
     path: 'chef',
+    canActivate: [roleGuard],
+    data: { roles: ['ROLE_ADMIN', 'ROLE_CHEF'] },
     loadComponent: () => import('./page/chef/chef').then((m) => m.Chef),
   },
   {
     path: 'admin',
+    canActivate: [roleGuard],
+    data: { roles: ['ROLE_ADMIN'] },
     loadComponent: () => import('./page/admin/admin').then((m) => m.Admin),
     children: [
       {
