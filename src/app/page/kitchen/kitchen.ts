2e9a0ce22498383d7b5cf7ccadd3afe5894003e1
Wasuda Sandaruvan <147605725+WasuVa@users.noreply.github.com>
2026-03-25 13:31:27 +0530

    feat: Update kitchen dashboard and order tables for improved styling and readability

--- src/app/page/kitchen/kitchen.ts
+++ src/app/page/kitchen/kitchen.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from "@angular/router";
+import { Sidebar } from "../sidebar/sidebar";
 
 @Component({
   selector: 'app-kitchen',
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet, Sidebar],
   templateUrl: './kitchen.html',
   styleUrl: './kitchen.css',
 })
